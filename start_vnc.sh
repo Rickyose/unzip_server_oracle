3a7a20776dd03eb2b9980980fcc7c6d855abb121
Rickyose <rickyhaterearged@gmail.com>
2021-09-20 20:37:12 +0700

    Update start_vnc.sh

--- start_vnc.sh
+++ start_vnc.sh
@@ -2,7 +2,7 @@
 #by pudh
 
 
-cp vnc_unzip_server.txt /home/ubuntu/.vnc/xstartup
-sudo -u root mv -f vnc.sh /home/ubuntu/
-chmod +x /home/ubuntu/vnc.sh
-/home/ubuntu/vnc.sh start :1
+
+#sudo -u root mv -f vnc.sh /home/ubuntu/
+#chmod +x /home/ubuntu/vnc.sh
+#/home/ubuntu/vnc.sh start :1
