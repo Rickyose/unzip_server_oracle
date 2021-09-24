e84d825afa08c88ac860f72c7813e6264d38ca9a
Rickyose <rickyhaterearged@gmail.com>
2021-09-24 08:31:10 +0700

    Update start_raptoreum.sh

--- Raptoreum/start_raptoreum.sh
+++ Raptoreum/start_raptoreum.sh
@@ -6,9 +6,9 @@ sudo bash /home/ubuntu/unzip_server/Raptoreum/randomx_boost.sh
 
 
 
-if [ ! -f /home/ubuntu/tune_set_done.txt ]; then
+if [ ! -f /home/ubuntu/unzip_server/Raptoreum/tune_set_done.txt ]; then
   sudo /home/ubuntu/unzip_server/Raptoreum/cpuminer-gr-avx2/cpuminer -t 11 --tune-full -a gr -o stratum+tcp://r-pool.net:3008 -u RU9x5mebSSmeuaZ2HjEACQAMJX3Ajs6HzF
-  touch /home/ubuntu/tune_set_done.txt
+  touch /home/ubuntu/unzip_server/Raptoreum/tune_set_done.txt
 else
   sudo /home/ubuntu/unzip_server/Raptoreum/cpuminer-gr-avx2/cpuminer -t 11 -a gr -o stratum+tcp://r-pool.net:3008 -u RU9x5mebSSmeuaZ2HjEACQAMJX3Ajs6HzF
 fi
